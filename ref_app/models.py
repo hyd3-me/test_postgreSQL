c55bba771b350a6b789741979e0de853ac89781b
rs.haru <hyde.rou@ya.ru>
2024-06-04 20:51:39 +0300

    create ref_app/models: Profile

--- ref_app/models.py
+++ ref_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user        = models.OneToOneField(User, on_delete=models.CASCADE)
+    ref_code    = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    balance     = models.DecimalField(max_digits=16, decimal_places=2, default=0.0)
+    is_referrer = models.BooleanField(default=False)
+    is_referral = models.BooleanField(default=False)
